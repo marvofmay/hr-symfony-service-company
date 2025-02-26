93a64163f02791c35d2ffc14e68cac24d97badc6
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-02-27 00:24:10 +0100

    Add enpoint - get industries list

--- src/src/Module/Company/Presentation/API/Controller/Industry/ListIndustriesController.php
+++ src/src/Module/Company/Presentation/API/Controller/Industry/ListIndustriesController.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace App\Module\Company\Presentation\API\Controller\Role;
+namespace App\Module\Company\Presentation\API\Controller\Industry;
 
-use App\Module\Company\Application\Query\Role\GetRolesQuery;
-use App\Module\Company\Application\QueryHandler\Role\GetRolesQueryHandler;
-use App\Module\Company\Domain\DTO\Role\RolesQueryDTO;
+use App\Module\Company\Application\Query\Industry\GetIndustriesQuery;
+use App\Module\Company\Application\QueryHandler\Industry\GetIndustriesQueryHandler;
+use App\Module\Company\Domain\DTO\Industry\IndustriesQueryDTO;
 use OpenApi\Attributes as OA;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +17,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-class ListRoleController extends AbstractController
+class ListIndustriesController extends AbstractController
 {
     public function __construct(
         private readonly LoggerInterface $logger,
@@ -27,21 +27,21 @@ class ListRoleController extends AbstractController
     }
 
     #[OA\Get(
-        path: '/api/roles',
-        summary: 'Pobiera listę ról',
+        path: '/api/industries',
+        summary: 'Pobiera listę branż',
         responses: [
             new OA\Response(
                 response: Response::HTTP_OK,
-                description: 'Lista ról',
+                description: 'Lista branż',
                 content: new OA\JsonContent(
                     properties: [
                         new OA\Property(property: 'data', properties: [
-                            new OA\Property(property: 'totalRoles', type: 'integer', example: 13),
+                            new OA\Property(property: 'totalIndustries', type: 'integer', example: 13),
                             new OA\Property(property: 'page', type: 'integer', example: 1),
                             new OA\Property(property: 'limit', type: 'integer', example: 10),
-                            new OA\Property(property: 'roles', type: 'array', items: new OA\Items(properties: [
+                            new OA\Property(property: 'industries', type: 'array', items: new OA\Items(properties: [
                                 new OA\Property(property: 'uuid', type: 'string', format: 'uuid', example: '9c1963a3-cb27-4e6a-b474-3509ed4b3457'),
-                                new OA\Property(property: 'name', type: 'string', example: 'rola 8'),
+                                new OA\Property(property: 'name', type: 'string', example: 'branża 8'),
                                 new OA\Property(property: 'description', type: 'string', example: 'Lorem ipsum dolor sit amet...'),
                                 new OA\Property(property: 'createdAt', type: 'string', format: 'date-time', example: '2025-02-09T18:56:07+00:00'),
                                 new OA\Property(property: 'updatedAt', type: 'string', format: 'date-time', example: '2025-02-10T10:42:55+00:00'),
@@ -54,13 +54,13 @@ class ListRoleController extends AbstractController
             ),
             new OA\Response(
                 response: Response::HTTP_INTERNAL_SERVER_ERROR,
-                description: 'Błąd przy pobieraniu listy ról',
+                description: 'Błąd przy pobieraniu listy branż',
                 content: new OA\JsonContent(
                     properties: [
                         new OA\Property(
                             property: 'error',
                             type: 'string',
-                            example: 'Wystapił błąd przy pobieraniu listy ról'
+                            example: 'Wystapił błąd przy pobieraniu listy branż'
                         ),
                     ],
                     type: 'object'
@@ -68,28 +68,28 @@ class ListRoleController extends AbstractController
             ),
         ]
     )]
-    #[OA\Tag(name: 'roles')]
-    #[Route('/api/roles', name: 'api.roles.list', methods: ['GET'])]
-    public function list(#[MapQueryString] RolesQueryDTO $queryDTO, GetRolesQueryHandler $usersQueryHandler): Response
+    #[OA\Tag(name: 'industries')]
+    #[Route('/api/industries', name: 'api.industries.list', methods: ['GET'])]
+    public function list(#[MapQueryString] IndustriesQueryDTO $queryDTO, GetIndustriesQueryHandler $usersQueryHandler): Response
     {
         try {
             return new JsonResponse([
                 'data' => json_decode($this->serializer->serialize(
-                    $usersQueryHandler->handle(new GetRolesQuery($queryDTO)),
-                    'json', ['groups' => ['role_info']],
+                    $usersQueryHandler->handle(new GetIndustriesQuery($queryDTO)),
+                    'json', ['groups' => ['industry_info']],
                 )),
             ],
                 Response::HTTP_OK
             );
         } catch (\Exception $error) {
             $this->logger->error(
-                sprintf('%s: %s', $this->translator->trans('role.list.error', [], 'roles'), $error->getMessage())
+                sprintf('%s: %s', $this->translator->trans('industry.list.error', [], 'industries'), $error->getMessage())
             );
 
             return new JsonResponse(
                 [
                     'data' => [],
-                    'message' => $this->translator->trans('role.list.error', [], 'roles'),
+                    'message' => $this->translator->trans('industry.list.error', [], 'industries'),
                 ],
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
