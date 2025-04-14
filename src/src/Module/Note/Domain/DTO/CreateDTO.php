f6f977eaaaf73be4baef1999c172a30296f1c474
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-04-14 13:58:40 +0200

    Remove openapi definitions from controllers

--- src/src/Module/Note/Domain/DTO/CreateDTO.php
+++ src/src/Module/Note/Domain/DTO/CreateDTO.php
@@ -6,22 +6,11 @@ namespace App\Module\Note\Domain\DTO;
 
 use App\Common\Validator\Constraints\NotBlank;
 use App\Module\Note\Domain\Trait\TitleContentPriorityTrait;
-use OpenApi\Attributes as OA;
 
-#[OA\Schema(
-    required: ['priority']
-)]
 class CreateDTO
 {
     use TitleContentPriorityTrait;
 
-    #[OA\Property(
-        description: 'UUID pracownika',
-        type: 'string',
-        format: 'uuid',
-        example: '550e8400-e29b-41d4-a716-446655440000',
-        nullable: false
-    )]
     #[NotBlank(message: [
         'text' => 'note.employeeUUID.required',
         'domain' => 'notes',
--- src/src/Module/Note/Presentation/API/Controller/GetNoteController.php
+++ src/src/Module/Note/Presentation/API/Controller/GetNoteController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Module\Note\Presentation\API\Controller;
 
-use App\Module\Note\Domain\Enum\NotePriorityEnum;
 use App\Module\Note\Domain\Interface\NoteReaderInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +11,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use OpenApi\Attributes as OA;
 
 #[Route('/api/notes', name: 'api.notes.')]
 class GetNoteController extends AbstractController
@@ -24,65 +22,6 @@ class GetNoteController extends AbstractController
     ) {
     }
 
-    #[OA\Get(
-        path: '/api/notes/{uuid}',
-        summary: 'Pobiera notatkę',
-        parameters: [
-            new OA\Parameter(
-                name: 'uuid',
-                description: 'UUID notatki',
-                in: 'path',
-                required: true,
-                schema: new OA\Schema(type: 'string')
-            ),
-        ],
-        responses: [
-            new OA\Response(
-                response: Response::HTTP_OK,
-                description: 'Notatka została pobrana',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(
-                            property: 'data',
-                            properties: [
-                                new OA\Property(property: 'uuid', type: 'string', format: 'uuid', example: '64a42efa-98ce-426a-9123-f44265ae96cc'),
-                                new OA\Property(property: 'title', type: 'string', example: 'Zadzwoń!!!'),
-                                new OA\Property(property: 'content', type: 'string', example: 'O 12:00 zadzwoń do przełożonego'),
-                                new OA\Property(
-                                    property: 'priority',
-                                    description: 'Priorytet tworzonej notatki (możliwe wartości: low, medium, high)',
-                                    type: 'string',
-                                    enum: [NotePriorityEnum::LOW, NotePriorityEnum::MEDIUM, NotePriorityEnum::HIGH],
-                                    example: NotePriorityEnum::HIGH,
-                                    nullable: false
-                                ),
-                                new OA\Property(property: 'createdAt', type: 'string', format: 'date-time', example: '2025-02-16T22:05:18+00:00'),
-                                new OA\Property(property: 'updatedAt', type: 'string', format: 'date-time', example: null, nullable: true),
-                                new OA\Property(property: 'deletedAt', type: 'string', format: 'date-time', example: null, nullable: true),
-                            ],
-                            type: 'object'
-                        ),
-                    ],
-                    type: 'object'
-                )
-            ),
-            new OA\Response(
-                response: Response::HTTP_INTERNAL_SERVER_ERROR,
-                description: 'Błąd niepoprawnego UUID',
-                content: new OA\JsonContent(
-                    properties: [
-                        new OA\Property(
-                            property: 'message',
-                            type: 'string',
-                            example: 'Wystapił błąd przy pobieraniu wybranej notatki: Notatka o podanym UUID nie istnieje : 64a42efa-98ce-426a-9123-f44265ae96c'
-                        ),
-                    ],
-                    type: 'object'
-                )
-            ),
-        ]
-    )]
-    #[OA\Tag(name: 'notes')]    
     #[Route('/{uuid}', name: 'get', methods: ['GET'])]
     public function get(string $uuid): JsonResponse
     {
