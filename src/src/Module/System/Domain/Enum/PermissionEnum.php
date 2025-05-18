619e88291ff0932c54deb50077e6ab2ec1362603
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-05-18 02:11:42 +0200

    Fix permission enum value

--- src/src/Module/System/Domain/Enum/PermissionEnum.php
+++ src/src/Module/System/Domain/Enum/PermissionEnum.php
@@ -12,7 +12,7 @@ enum PermissionEnum: string implements EnumInterface
     case VIEW = 'view';
     case LIST = 'list';
     case IMPORT = 'import';
-    case ASSIGN_PERMISSION_TO_ACCESS_ROLE = 'assign_access_to_access_role';
+    case ASSIGN_PERMISSION_TO_ACCESS_ROLE = 'assign_permission_to_access_role';
     case ASSIGN_ACCESS_TO_ROLE = 'assign_access_to_role';
 
     public function label(): string
