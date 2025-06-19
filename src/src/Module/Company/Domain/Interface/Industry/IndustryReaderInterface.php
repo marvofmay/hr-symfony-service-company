8f71c1390b2f6def1641fb5d712dc17650816b7d
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-06-19 12:52:39 +0200

    Refactor industries - add method to readerInterface

--- src/src/Module/Company/Domain/Interface/Industry/IndustryReaderInterface.php
+++ src/src/Module/Company/Domain/Interface/Industry/IndustryReaderInterface.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Module\Company\Domain\Interface\Industry;
 
 use App\Module\Company\Domain\Entity\Industry;
+use Doctrine\Common\Collections\Collection;
 
 interface IndustryReaderInterface
 {
     public function getIndustryByUUID(string $uuid): ?Industry;
+    public function getIndustriesByUUID(array $selectedUUID): Collection;
 
     public function getIndustryByName(string $name, ?string $uuid): ?Industry;
+    public function isIndustryExists(string $name, ?string $uuid = null): bool;
+    public function isIndustryExistsWithUUID(string $uuid): bool;
 }
