16789ff0a343820e7184d7bcb9e8d863d8c393f5
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-06-20 10:26:52 +0200

    Change .gitignore

--- src/src/Module/Company/Domain/DTO/Industry/DeleteMultipleDTO.php
+++ src/src/Module/Company/Domain/DTO/Industry/DeleteMultipleDTO.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Module\Company\Domain\DTO\Industry;
 
-use App\Module\Company\Structure\Validator\Constraints\Industry\ExistingIndustryUUID;
 use Symfony\Component\Validator\Constraints as Assert;
 
 final class DeleteMultipleDTO
