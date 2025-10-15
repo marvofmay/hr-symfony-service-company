8d71b1ee9fb1422265502134f9b13e2c0033460a
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-10-15 22:27:32 +0200

    Add JWT invalid event listener

--- src/src/Module/System/Application/EventListener/ExpiredJwtListener.php
+++ src/src/Module/System/Application/EventListener/ExpiredJwtListener.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Module\System\Application\EventListener;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
-use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTInvalidEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[AsEventListener(event: 'lexik_jwt_authentication.on_jwt_expired', method: 'onJWTExpired')]
-#[AsEventListener(event: 'lexik_jwt_authentication.on_jwt_invalid', method: 'onJWTInvalid')]
 readonly class ExpiredJwtListener
 {
     public function __construct(private TranslatorInterface $translator)
@@ -23,16 +21,6 @@ readonly class ExpiredJwtListener
         $response = new JsonResponse([
             'code' => 401,
             'message' => $this->translator->trans('token.expired', [], 'security'),
-        ], 401);
-
-        $event->setResponse($response);
-    }
-
-    public function onJWTInvalid(JWTInvalidEvent $event): void
-    {
-        $response = new JsonResponse([
-            'code' => 401,
-            'message' => $this->translator->trans('token.invalid', [], 'security'),
         ], 401);
 
         $event->setResponse($response);
