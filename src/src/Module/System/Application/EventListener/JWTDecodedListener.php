9cce07030f3377ffacf93a956b4419264b2087e1
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-11-07 14:22:53 +0100

    Add jwtDecode listener

---
+++ src/src/Module/System/Application/EventListener/JWTDecodedListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Module\System\Application\EventListener;
+
+use App\Module\System\Domain\Service\AuthEvent\BlacklistTokenService;
+use App\Module\System\Domain\ValueObject\TokenUUID;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener(event: 'lexik_jwt_authentication.on_jwt_decoded', method: 'onJWTDecoded')]
+final readonly class JWTDecodedListener
+{
+    public function __construct(private BlacklistTokenService $blacklist) {}
+
+    public function onJWTDecoded(JWTDecodedEvent $event): void
+    {
+        $payload = $event->getPayload();
+        if (!isset($payload['tokenUUID'])) {
+            $event->markAsInvalid();
+
+            return;
+        }
+
+        $tokenUUID = $payload['tokenUUID'];
+        if ($this->blacklist->isRevoked(TokenUUID::fromString($tokenUUID))) {
+            $event->markAsInvalid();
+        }
+    }
+}
