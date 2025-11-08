3929d617f0ee82d7edc0457cbda9860e7e5282d0
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-11-09 00:30:28 +0100

    Remove comments

--- src/src/Module/Note/Infrastructure/DI/LoggableEventsPass.php
+++ src/src/Module/Note/Infrastructure/DI/LoggableEventsPass.php
@@ -25,13 +25,8 @@ final class LoggableEventsPass implements CompilerPassInterface
             ? $container->getDefinition($listenerServiceId)
             : $container->getAlias($listenerServiceId);
 
-        // dla każdego odnalezionego serwisu -> dodajemy tag kernel.event_listener
         foreach ($tagged as $eventServiceId => $tags) {
-            // tutaj $eventServiceId to id serwisu eventu; chcemy klasę eventu = $eventServiceId
-            // ale findTaggedServiceIds zwraca identyfikatory usług; jeśli eventy są rejestrowane jako klasa -> id == klasa
             $eventClass = $eventServiceId;
-
-            // do listenera dodajemy tag event listener dla zdarzenia $eventClass
             $definition->addTag('kernel.event_listener', [
                 'event'  => $eventClass,
                 'method' => 'onLoggableEvent',
