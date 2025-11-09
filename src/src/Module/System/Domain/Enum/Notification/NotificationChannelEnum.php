686726fb5e29109007a7f7e79ae7ecaae22420d9
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2025-11-09 09:02:07 +0100

    Add interface to enum and translations

--- src/src/Module/System/Domain/Enum/Notification/NotificationChannelEnum.php
+++ src/src/Module/System/Domain/Enum/Notification/NotificationChannelEnum.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Module\System\Domain\Enum\Notification;
 
-enum NotificationChannelEnum: string
+use App\Common\Domain\Interface\EnumInterface;
+
+enum NotificationChannelEnum: string implements EnumInterface
 {
     case INTERNAL = 'internal';
     case EMAIL = 'email';
     case SMS = 'sms';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::INTERNAL => 'notification.channel.internal',
+            self::EMAIL => 'notification.channel.email',
+            self::SMS => 'notification.channel.sms',
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_map(fn(self $case) => $case->value, self::cases());
+    }
 }
